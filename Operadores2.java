4ed1c6f0677a3c67061483a1db91750080c15470
Ana Nogueira <143566337+ananogs-bot@users.noreply.github.com>
2024-10-10 11:04:17 -0300

    Add files via upload

---
+++ Operadores2.java
@@ -0,0 +1,19 @@
+package Operadores;
+
+public class Operadores2 {
+
+	public static void main(String[] args) {
+		
+		int numero = 5;
+		
+		//x repetição
+		numero++; // é igual a número + 1
+		
+		numero += 1; // é igual a anterior
+		
+		System.out.println(numero++); //adiciona 1 depois 
+		
+		System.out.println(++numero); //adiciona 1 antes
+	}
+
+}
